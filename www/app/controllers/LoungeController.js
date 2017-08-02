a654e227c76f49d8c565c4883ce342a879544ada
Roiner994 <Roiner123@gmail.com>
2017-08-02 14:35:08 -0400

    retcando vistas

--- www/app/controllers/LoungeController.js
+++ www/app/controllers/LoungeController.js
@@ -211,6 +211,20 @@ app.controller('LoungeController', ['$scope', '$state', '$http','$stateParams','
 				url: server_uri+'/products/'+$.sessionStorage.get('longe_id'),
 			}).then(function successCallback(response) {
 				$scope.Productos=response.data;
+				$timeout(function(){
+					$('.modal').modal();
+			       	$('.dropdown-button').dropdown({
+					      inDuration: 300,
+					      outDuration: 225,
+					      constrainWidth: false, // Does not change width of dropdown to that of the activator
+					      hover: true, // Activate on hover
+					      gutter: 0, // Spacing from edge
+					      belowOrigin: true, // Displays dropdown below the button
+					      alignment: 'left', // Displays dropdown with edge aligned to the left of button
+					      stopPropagation: false // Stops event propagation
+					    }
+					);
+			    });
 			}, function errorCallback(response) {
 				console.log('dio error');
 			});
@@ -242,6 +256,7 @@ app.controller('LoungeController', ['$scope', '$state', '$http','$stateParams','
 			$scope.Producto.lounge_id=$.sessionStorage.get('longe_id');
 
 			$scope.registrarProducto=function(){
+				console.log($scope.Producto);
 				$http({
 					method: 'POST',
 					url: server_uri+'/products',
@@ -294,6 +309,20 @@ app.controller('LoungeController', ['$scope', '$state', '$http','$stateParams','
 				url: server_uri+'/loungeServices/'+$.sessionStorage.get('longe_id'),
 			}).then(function successCallback(response) {
 				$scope.Servicios=response.data;
+				$timeout(function(){
+					$('.modal').modal();
+			       	$('.dropdown-button').dropdown({
+					      inDuration: 300,
+					      outDuration: 225,
+					      constrainWidth: false, // Does not change width of dropdown to that of the activator
+					      hover: true, // Activate on hover
+					      gutter: 0, // Spacing from edge
+					      belowOrigin: true, // Displays dropdown below the button
+					      alignment: 'left', // Displays dropdown with edge aligned to the left of button
+					      stopPropagation: false // Stops event propagation
+					    }
+					);
+			    });
 			}, function errorCallback(response) {
 				console.log('dio error');
 			});
